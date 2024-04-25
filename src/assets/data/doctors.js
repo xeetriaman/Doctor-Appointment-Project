fd36fc01de5a2e360042b10ada870dd4d86050a8
xeetriaman <77188934+xeetriaman@users.noreply.github.com>
2024-04-25 13:10:03 +0545

    Frontend

---
+++ src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Cherry Gyawali",
+    specialization: "Gynacologist",
+    avgRating: 4.2,
+    totalRating: 212,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "Norvic Hospital, Kathmandu.",
+  },
+  {
+    id: "02",
+    name: "Dr. Aman Chhetri",
+    specialization: "Neurologist",
+    avgRating: 4.9,
+    totalRating: 372,
+    photo: doctorImg02,
+    totalPatients: 1000,
+    hospital: "Medicity Hospital, Lalitpur.",
+  },
+  {
+    id: "03",
+    name: "Dr. Rishav Thapa",
+    specialization: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 333,
+    photo: doctorImg03,
+    totalPatients: 2500,
+    hospital: "Bir Hospital, Kathmandu.",
+  },
+];
